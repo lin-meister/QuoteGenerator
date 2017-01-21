469c2856ff05d27ec0f60746120767e0a925803c
lin-meister <matthewcaus@gmail.com>
2017-01-20 17:37:01 -0800

    Finished core functionality of quote generation in MainActivity

--- Quote Generator Skeleton/app/src/main/java/com/acmhack/ucla/quotegenerator/MainActivity.java
+++ Quote Generator Skeleton/app/src/main/java/com/acmhack/ucla/quotegenerator/MainActivity.java
@@ -1,44 +1,96 @@
 package com.acmhack.ucla.quotegenerator;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     ImageButton smallberg_button, nachenberg_button, miodrag_button;
-    Button generate;
+    Button generate_button;
     TextView smallberg_text, nachenberg_text, miodrag_text, quote_text;
     //If active professor is 0 => Smallberg, 1 => Nachenberg, 2 => Miodrag
     int active_professor;
+    int active_quote;
     //Add your own quotes here!
     //FUN FACT: miodrag's tumblr has some very "interesting" quotes http://pot-cognac.tumblr.com/
-    String [] smallberg_quotes = {"\"Don’t forget the semicolon\""};
-    String [] nachenberg_quotes = {"\"I need a volunteer. Yes, you—if it starts smoking, tell me!\""};
-    String [] miodrag_quotes = {"\"Don’t get all excited when you find problems easy; your peers will find them easy too\""};
+    String [] smallberg_quotes = {
+            "\"Don’t forget the semicolon\""
+    };
+    String [] nachenberg_quotes = {
+            "\"I need a volunteer. Yes, you—if it starts smoking, tell me!\"",
+            "\"Can I call you Bob?\""
+    };
+    String [] miodrag_quotes = {
+            "\"Don’t get all excited when you find problems easy; your peers will find them easy too\"",
+            "\"I don’t like screaming it is too loud\""
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Set up Image Buttons
+        smallberg_button = (ImageButton) findViewById(R.id.smallberg_button);
+        nachenberg_button = (ImageButton) findViewById(R.id.nachenberg_button);
+        miodrag_button = (ImageButton) findViewById(R.id.miodrag_button);
 
         //Set up Buttons
+        generate_button = (Button) findViewById(R.id.generate_button);
 
         //Set up Text Views
+        smallberg_text = (TextView) findViewById(R.id.smallberg_text);
+        nachenberg_text = (TextView) findViewById(R.id.nachenberg_text);
+        miodrag_text = (TextView) findViewById(R.id.miodrag_text);
+        quote_text = (TextView) findViewById(R.id.quote_text);
 
         //Set up On Click Listeners
+        generate_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Set all the professor text colors to black
+                setAllProfessorToBlackText();
+                // Choose a random professor
+                Random rand = new Random();
+                active_professor = rand.nextInt(3); // Gives n such that 0 <= n < 3
 
+                switch (active_professor) {
+                    // Case 0: Professor will be Smallberg, generate a random quote from him
+                    case 0:
+                        active_quote = rand.nextInt(smallberg_quotes.length);
+                        quote_text.setText(smallberg_quotes[active_quote]);
+                        smallberg_text.setTextColor(Color.parseColor("#FDC511"));
+                        break;
+                    // Case 1: Professor will be Nachenberg, generate a random quote from him
+                    case 1:
+                        active_quote = rand.nextInt(nachenberg_quotes.length);
+                        quote_text.setText(nachenberg_quotes[active_quote]);
+                        nachenberg_text.setTextColor(Color.parseColor("#FDC511"));
+                        break;
+                    // Case 2: Professor will be Niodrag, generate a random quote from him
+                    case 2:
+                        active_quote = rand.nextInt(miodrag_quotes.length);
+                        quote_text.setText(miodrag_quotes[active_quote]);
+                        miodrag_text.setTextColor(Color.parseColor("#FDC511"));
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
     }
 
     //free optional helper function, for resetting all professors to black text
-    /*
     public void setAllProfessorToBlackText(){
         smallberg_text.setTextColor(Color.BLACK);
         nachenberg_text.setTextColor(Color.BLACK);
         miodrag_text.setTextColor(Color.BLACK);
     }
-    */
+
 }
